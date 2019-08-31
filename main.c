e72d5f4787a01766398e237d1727f4a09c8b2db4
imgabe <gabrielpmonte@hotmail.com>
2019-08-30 22:24:15 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+
+void centralizeColors(int col) {
+  char text[121];
+  char color[17] = "";
+  strcpy(text, "");
+
+  for (int i = 40; i < 48; ++i) {
+    sprintf(color, "\033[%dm      \033[0m  ", i);
+    strcat(text, color);
+  }
+
+  int padlen = (col - 64) / 2;
+  printf("%*s%s%*s\n", padlen, "", text, padlen, "");
+  printf("%*s%s%*s\n", padlen, "", text, padlen, "");
+  printf("\n");
+}
+
+int main() {
+  struct winsize win;
+  ioctl(0, TIOCGWINSZ, &win);
+  centralizeColors(win.ws_col);
+  return 0;
+}
